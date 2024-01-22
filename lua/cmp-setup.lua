512636408204dff26ed0736ca5c1620e8ff89fc0
gmankab <gmankab@gmail.com>
2024-01-22 09:39:07 +0400

    [+] scroll by ctrl+j and ctrl+k in nvim-cmp

--- lua/cmp-setup.lua
+++ lua/cmp-setup.lua
@@ -11,14 +11,13 @@ cmp.setup {
       luasnip.lsp_expand(args.body)
     end,
   },
-  completion = {
-    completeopt = 'menu,menuone,noinsert',
-  },
   mapping = cmp.mapping.preset.insert {
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
     ['<C-Space>'] = cmp.mapping.complete {},
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
@@ -49,5 +48,5 @@ cmp.setup {
     { name = 'path' },
   },
 }
-
 -- vim: ts=2 sts=2 sw=2 et
+
