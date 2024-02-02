5c6ad80233eedf3c6b43cc06ddb8050e1bc43e9a
gmankab <gmankab@gmail.com>
2024-02-02 08:42:49 +0400

    [+] added open docs bind

--- lua/cmp-setup.lua
+++ lua/cmp-setup.lua
@@ -16,6 +16,7 @@ cmp.setup {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-s>'] = cmp.mapping.open_docs(),
     ['<C-j>'] = cmp.mapping.select_next_item(),
     ['<C-k>'] = cmp.mapping.select_prev_item(),
     ['<C-Space>'] = cmp.mapping.complete {},
