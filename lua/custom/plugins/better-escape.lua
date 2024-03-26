4f3bef92a6ee33fe4c94d96e37fe58d469cf2461
gmankab <gmankab@gmail.com>
2024-03-26 08:19:53 +0400

    [+] delte trailing spaces on save

--- lua/custom/plugins/better-escape.lua
+++ lua/custom/plugins/better-escape.lua
@@ -1,5 +1,5 @@
 return {
-  'max397574/better-escape.nvim',
+  'Sam-programs/better-escape.nvim',
   version = '*',
   config = function ()
     require('better_escape').setup {
@@ -7,20 +7,33 @@ return {
       timeout = vim.o.timeoutlen,
       clear_empty_lines = false,
       keys = '<Esc>',
-      -- example
-      -- keys = function()
-      --   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
-      -- end,
     }
     -- autosave file
-    vim.api.nvim_create_autocmd({'InsertLeave', 'TextChanged'}, {
+    vim.api.nvim_create_autocmd({'InsertLeave'}, {
       pattern = '*',
       callback = function()
-        vim.defer_fn(function()
-          if vim.api.nvim_buf_get_option(0, 'buftype') == '' then
-            vim.cmd('silent update')
-          end
-        end, 10)
+        vim.defer_fn(
+          function()
+            if vim.api.nvim_buf_get_option(0, 'buftype') == '' then
+              local current_view = vim.fn.winsaveview()
+              vim.cmd([[keeppatterns %s/\s\+$//e]])
+              vim.fn.winrestview(current_view)
+              vim.cmd('silent update')
+            end
+          end, 10
+        )
+      end
+    })
+    vim.api.nvim_create_autocmd({'TextChanged'}, {
+      pattern = '*',
+      callback = function()
+        vim.defer_fn(
+          function()
+            if vim.api.nvim_buf_get_option(0, 'buftype') == '' then
+              vim.cmd('silent update')
+            end
+          end, 10
+        )
       end
     })
   end,
