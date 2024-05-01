0b2177d96319e503f32a498f353eaa4a72ee240b
gmankab <gmankab@gmail.com>
2024-05-01 05:52:52 +0400

    [+] icons

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -75,6 +75,7 @@ require('lazy').setup({
   -- Fuzzy Finder (files, lsp, etc)
   {
     'nvim-telescope/telescope.nvim',
+    'nvim-tree/nvim-web-devicons',
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
