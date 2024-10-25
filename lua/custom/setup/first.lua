a6636f3a38d07629d034a6adede074f0f3c38c9e
gmanka <gmankab@gmail.com>
2024-10-25 04:42:39 +0400

    [u] font size

--- lua/custom/setup/first.lua
+++ lua/custom/setup/first.lua
@@ -1,5 +1,5 @@
 vim.g.neovide_hide_mouse_when_typing = true
-vim.o.guifont = 'JetBrainsMono Nerd Font:h15'
+vim.o.guifont = 'JetBrainsMono Nerd Font:h18'
 vim.opt.relativenumber = true
 vim.fn.setenv('EDITOR', 'nvim')
 
