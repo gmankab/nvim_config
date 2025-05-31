6a46fe735cac76e0b24dd3a7d9769f9f7cbda686
gmanka <gmankab@gmail.com>
2025-05-31 10:06:10 +0500

    [-] delete colorscheme

--- init.lua
+++ init.lua
@@ -896,6 +896,7 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
+    enabled=false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
@@ -908,7 +909,9 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      --[[
       vim.cmd.colorscheme 'tokyonight-night'
+      ]]
     end,
   },
 
--- lua/custom/plugins/gruvbox.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'sainnhe/gruvbox-material',
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme 'gruvbox-material'
-  end,
-}
-
