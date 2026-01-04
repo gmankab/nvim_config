a87d0529ece675122922ce89f422304de47e4ea6
gmanka <gmankab@gmail.com>
2026-01-05 00:47:08 +0500

    [+] config for ty

--- init.lua
+++ init.lua
@@ -1031,6 +1031,7 @@ require('lazy').setup({
 
 require 'custom.setup.autosave'
 require 'custom.setup.keymaps'
+require 'custom.setup.lsp'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
