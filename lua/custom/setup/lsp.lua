73844351f5c5a92d821b18261a1f620ea55e113c
gmanka <gmankab@gmail.com>
2026-03-08 07:34:13 +0500

    [+] enable some lsps

--- lua/custom/setup/lsp.lua
+++ lua/custom/setup/lsp.lua
@@ -9,5 +9,18 @@ vim.lsp.config('ty', {
     },
   },
 })
-vim.lsp.enable('ty')
-vim.lsp.enable('ruff')
+local function lsp_report()
+  vim.wait(
+    3000,
+    function()
+      return #vim.diagnostic.get(0) > 0
+    end,
+    100
+  )
+  vim.print(vim.diagnostic.get(0))
+end
+
+vim.api.nvim_create_user_command('LspReport', lsp_report, {})
+for _, server in ipairs { 'ty', 'ruff', 'gh_actions_ls', 'yamlls' } do
+  vim.lsp.enable(server)
+end
